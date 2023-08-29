677249b18488dd45e7f9508554d53935989ccc57
Christian <christian@juenger.io>
2023-08-29 21:20:47 +0200

    Save intermediate result

---
+++ src/Pippin/Pipes/PipeEnd.cs
@@ -0,0 +1,9 @@
+﻿using Pippin.Filters;
+
+namespace Pippin.Pipes
+{
+    public abstract class PipeEnd<TInput> : IFilterInput<TInput>
+    {
+        public abstract void Input(TInput input);
+    }
+}
---
+++ src/Pippin/Pipes/PipeStart.cs
@@ -0,0 +1,13 @@
+﻿using Pippin.Filters;
+
+namespace Pippin.Pipes
+{
+    public abstract class PipeStart<TOutput>
+    {
+        /// <summary>
+        /// Chains a filter behind this start of the pipe.
+        /// </summary>
+        /// <param name="filter">A filter to be chained behind this filter</param>
+        public abstract void ChainFilter(IFilterInput<TOutput> filter);
+    }
+}
